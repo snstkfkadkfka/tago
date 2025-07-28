e94130e3439c1f7cf5e91a1d0c4307ac3d62b98b
snstkfkadkfka <qotnrqks@naver.com>
2025-07-28 13:37:35 +0900

    pw

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-// publicPath:"TagoPortfolio",
+publicPath: "/togo/",
 chainWebpack: config => {
 
     config.plugin('define').tap(definitions => {
