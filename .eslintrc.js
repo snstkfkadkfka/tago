741bb6997ca73aedd14423ae088a134722246e71
snstkfkadkfka <qotnrqks@naver.com>
2025-07-28 13:57:28 +0900

    Updates

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+
+  },
+  globals: {
+    naver: 'readonly'
+  }
+}
